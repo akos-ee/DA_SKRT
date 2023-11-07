e7b234407bca9e54b785d12883a4f3ca9e4dac96
akos-ee <74436477+akos-ee@users.noreply.github.com>
2023-11-07 16:56:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import this
+
 from Utilities.incident_angle import WaveAnalyzer
 import os
+import numpy
+
+num = 0
 def get3chanarr(folder):
+    global num
     list = os.listdir(folder)
     i=0
     j=0
@@ -12,6 +18,8 @@ def get3chanarr(folder):
     while i< len(list):
         if 'Audio Track' in list[i] and j < 3:
             arrs[j] = ((WaveAnalyzer((folder + "/" + list[i]))).wav_to_numpy_array())[1]
+            if abs(max(arrs[j]))>num:
+                num = abs(max(arrs[j]))
             j = j+1
 
         i = i+1
@@ -22,9 +30,8 @@ if __name__ == '__main__':
     # x = analyzer.wav_to_numpy_array()
     # sig = x[1]
 
-
-    sig = get3chanarr("/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities")
-    print(sig)
+    sig = get3chanarr("/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities")[2]
+    print(num)
 
 
 
