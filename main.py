96f3ed2df07bc306d471a1ad277c170511d532d3
akos-ee <74436477+akos-ee@users.noreply.github.com>
2023-11-07 16:10:18 -0500

    lit

--- main.py
+++ main.py
@@ -3,16 +3,27 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from Utilities.incident_angle import WaveAnalyzer
+import os
+def get3chanarr(folder):
+    list = os.listdir(folder)
+    i=0
+    j=0
+    arrs = [None, None, None]
+    while i< len(list):
+        if 'Audio Track' in list[i]:
+            arrs[j] = ((WaveAnalyzer((folder + "/" + list[i]))).wav_to_numpy_array())[i]
+            j = j+1
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+        i = i+1
 if __name__ == '__main__':
-    wav_path = '/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities/test_sound.wav'  # Replace with your WAV file path
+    wav_path = '/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities/led ring 1kHz 0deg.wav'  # Replace with your WAV file path
     analyzer = WaveAnalyzer(wav_path)
-    analyzer.play()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    x = analyzer.wav_to_numpy_array()
+    sig = x[1]
+    print(sig)
+
+
+
+
+
+
