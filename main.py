8c473ca008568d28e55ce3a00cb531a6217c195d
akos-ee <74436477+akos-ee@users.noreply.github.com>
2023-11-07 16:23:16 -0500

    Update main.py
    Function that gets array from 3 different wav files

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ def get3chanarr(folder):
     j=0
     arrs = [None, None, None]
     while i< len(list):
-        if 'Audio Track' in list[i]:
-            arrs[j] = ((WaveAnalyzer((folder + "/" + list[i]))).wav_to_numpy_array())[i]
+        if 'Audio Track' in list[i] and j < 3:
+            arrs[j] = ((WaveAnalyzer((folder + "/" + list[i]))).wav_to_numpy_array())[1]
             j = j+1
 
         i = i+1
+    return arrs
 if __name__ == '__main__':
-    wav_path = '/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities/led ring 1kHz 0deg.wav'  # Replace with your WAV file path
-    analyzer = WaveAnalyzer(wav_path)
-    x = analyzer.wav_to_numpy_array()
-    sig = x[1]
+    # wav_path = '/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities/led ring 1kHz 0deg.wav'  # Replace with your WAV file path
+    # analyzer = WaveAnalyzer(wav_path)
+    # x = analyzer.wav_to_numpy_array()
+    # sig = x[1]
+
+
+    sig = get3chanarr("/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities")
     print(sig)
 
 
