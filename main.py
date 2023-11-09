c649fe46d2d46db538e207876de09e059d489e4a
akos-ee <74436477+akos-ee@users.noreply.github.com>
2023-11-09 16:29:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,14 @@ def get3chanarr(folder):
     while i< len(list):
         if 'Audio Track' in list[i] and j < 3:
             arrs[j] = ((WaveAnalyzer((folder + "/" + list[i]))).wav_to_numpy_array())[1]
-            if abs(max(arrs[j]))>num:
+            if abs(max(arrs[j])) > num:
                 num = abs(max(arrs[j]))
             j = j+1
 
         i = i+1
+    arrs[0] = (arrs[0] /num)+1  # normalize arrays and shift
+    arrs[1] = (arrs[1] / num)+1
+    arrs[2] = (arrs[2] / num)+1
     return arrs
 if __name__ == '__main__':
     # wav_path = '/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities/led ring 1kHz 0deg.wav'  # Replace with your WAV file path
@@ -31,8 +34,7 @@ if __name__ == '__main__':
     # sig = x[1]
 
     sig = get3chanarr("/Users/akosborbath/Documents/Soundskrit/DA/DA_SKRT/Utilities")[2]
-    print(num)
-
+    print(sig)
 
 
 
